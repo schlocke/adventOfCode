5a691c811ce8e61ac2d62feebb4992d5c617c58e
johnathon rausch <johnathon rausch>
2016-12-14 08:16:28 -0500

    Signed-off-by: johnathon rausch <johnathon rausch>

---
+++ PHP2016/day10.php
@@ -0,0 +1,27 @@
+<?php
+
+$input = file('day10.txt');
+
+$bot = $instr = $output = $values = [];
+$id = 0;
+
+foreach ($input as $k => $row)
+       (preg_match('/^v.*?(\d+).*?(\d+)$/', $row, $m) && $bot[$m[2]][] = $m[1])
+    || (preg_match('/^b.*?(\d+).*?(bot|output)\s(\d+).*?(bot|output)\s(\d+)$/', $row, $m) && $instr[$m[1]] = ['min' => $m[2] . '.' . $m[3], 'max' => $m[4] . '.' . $m[5]]);
+
+while (true) {
+    foreach ($bot as $id => $values) if (count($values) == 2) break;
+    if (count($values) != 2) break;
+
+    if (in_array(61, $bot[$id]) && in_array(17, $bot[$id])) echo 'BOT ID: ' . $id . PHP_EOL;
+
+    foreach ($instr[$id] as $fn => $target)
+        (list($target, $number) = explode('.', $target)) && ${$target}[$number][] = $fn($bot[$id]);
+
+    $bot[$id] = [];
+}
+
+var_dump($input);
+var_dump($output);
+
+echo 'Product: ' . ($output[0][0] * $output[1][0] * $output[2][0]) . PHP_EOL;
